ff35e35baffc4e7ccb79a865c50174a560340ce8
Barak Setton <barak@luno.com>
2022-01-25 20:16:56 +0200

    update headers

--- js/scripts.js
+++ js/scripts.js
@@ -245,11 +245,7 @@
       url: 'https://g6gtqb4crj.execute-api.us-west-2.amazonaws.com/default/addToWaitlist',
       data: JSON.stringify(data),
       dataType: 'json',
-      ContentType: 'application/json',
       headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
-        'Access-Control-Allow-Credentials': true,
         'Content-Type': 'application/json',
       },
       success: function(text) {
